56a97d41faa199ff407aec581beeaee27d4994bd
Timothy Danford <tdanford@gmail.com>
2010-09-30 16:37:43 -0400

    Initial check-in.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Parsing, exploring, displaying, and searching the Cell Type Ontology (CL) from OBO Foundry.
+
+http://www.obofoundry.org/cgi-bin/detail.cgi?id=cell
---
+++ java/src/org/sc/celltype/CellTypeOntology.java
@@ -0,0 +1,10 @@
+package org.sc.celltype;
+
+import java.io.*;
+
+public class CellTypeOntology { 
+	
+	public CellTypeOntology(File f) throws IOException { 
+			
+	}
+}

98241bbf3201a2d29aaea0813a9319f51ea358fc
Timothy Danford <tdanford@gmail.com>
2010-10-27 10:25:28 -0400

    Updated to return develops_into and is_a notation.
    Working on updating the servlet so that it can drive a jsTree-style interface for SCF.  The servlet now does the sub/superclass computation, and the develops_from graph relationships, and correctly computes the complete "develops_into" relationships from a starting cell type.  This involves following the develops_into relationships from either a term itself, or the subclasses of the term.
    
    The servlet is now outputting text; the next change will be to update it to use JSON.

--- java/src/org/sc/celltype/servlets/CellTypeOntologyServlet.java
+++ java/src/org/sc/celltype/servlets/CellTypeOntologyServlet.java
@@ -1,20 +1,72 @@
 package org.sc.celltype.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URLDecoder;
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 import org.sc.celltype.CellTypeOntology;
+import org.sc.celltype.CycleDetectedException;
+import org.sc.celltype.DAG;
 import org.sc.obo.OBOTerm;
 
 public class CellTypeOntologyServlet extends HttpServlet {
 	
 	private CellTypeOntology ontology;
+	private DAG isaDag, subclassDag, developsIntoDag;
 	
 	public CellTypeOntologyServlet(CellTypeOntology ont) { 
 		ontology = ont;
+	
+		isaDag = ontology.getIsaDag();
+		
+		try {
+			isaDag.calculateConnectivity();
+		} catch (CycleDetectedException e) {
+			throw new IllegalArgumentException("Cycle detected in ontology is_a graph.");
+		}
+		subclassDag = isaDag.flipEdges();
+		developsIntoDag = ontology.getDevelopsFromDag().flipEdges();
+	}
+	
+	public CellTypeOntologyServlet() throws IOException { 
+		this(new CellTypeOntology());
+	}
+	
+	public Set<String> reduceByIsa(Collection<String> ids) {
+		String[] base = ids.toArray(new String[0]);
+		TreeSet<String> reduced = new TreeSet<String>(ids);
+		
+		// do nothing, for now.
+		
+		return reduced;
+	}
+	
+	public void printDevelopsGraph(PrintWriter writer, int depth, String id, String suffix) { 
+		for(int i = 0; i < depth; i++) { 
+			writer.print("  ");
+		}
+		
+		OBOTerm developsIntoTerm = ontology.getTerm(id);
+		writer.println(String.format("%s ! %s\t%s", id, developsIntoTerm.getName(), suffix));
+
+		for(String next : developsIntoDag.edges(id)) { 
+			printDevelopsGraph(writer, depth+1, next, "");
+		}
+		
+		for(String subclass : subclassDag.edges(id)) { 
+			OBOTerm subclassTerm = ontology.getTerm(subclass);
+			for(String next : developsIntoDag.edges(subclass)) { 
+				printDevelopsGraph(writer, depth+1, next,
+						"");
+						//String.format("(via %s ! %s)", subclass, subclassTerm.getName()));				
+			}
+		}
 	}
 
 	/**
@@ -39,6 +91,30 @@ public class CellTypeOntologyServlet extends HttpServlet {
 
 		response.setStatus(HttpServletResponse.SC_OK);
 		response.setContentType("text");
-		oboTerm.print(response.getWriter());
+		PrintWriter writer = response.getWriter();
+		
+		//oboTerm.print(writer);
+		
+		writer.println(oboTerm.getName());
+		
+		Set<String> developsInto = new TreeSet<String>(developsIntoDag.edges(term));
+		
+		for(String isaID : isaDag.edges(term)) { 
+			OBOTerm isaTerm = ontology.getTerm(isaID);
+			writer.println(String.format("is_a: %s", isaTerm.getName()));
+			//developsInto.addAll(developsIntoDag.edges(isaID));
+		}
+		
+		printDevelopsGraph(writer, 0, term, "");
+		
+		//developsInto = reduceByIsa(developsInto);
+
+		/*
+		for(String developsIntoID : developsInto) { 
+			OBOTerm developsIntoTerm = ontology.getTerm(developsIntoID);
+			writer.println(String.format("develops_into: %s", developsIntoTerm.getName()));			
+		}
+		*/
+		
 	}
 }
